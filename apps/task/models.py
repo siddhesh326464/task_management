afb4416f7cb3c9f60581d7cb1cb3e2ec49c440cd
siddhesh326464 <109015040+siddhesh326464@users.noreply.github.com>
2024-06-29 00:15:27 +0530

    feat : models defined

--- apps/task/models.py
+++ apps/task/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+class Project:
+    class Meta:
+        db_table = "project"
+        verbose_name = "Projects"
+        verbose_name_plural = "Project"
+    name = models.CharField(max_length=50,unique=True,null=True,blank=True)
+    user = models.ForeignKey('Account',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post:
+    class Meta:
+        db_table = "post"
+        verbose_name = "Posts"
+        verbose_name_plural = "Post"
+    title = models.CharField(max_length=50,null=True,blank=True)
+    detail = models.TextField(null=True,blank=True)
+    user = models.ForeignKey('Account',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey('Account',on_delete=models.CASCADE)
+    updated_by = models.ForeignKey('Account',on_delete=models.CASCADE)
+
+class UserPostAssociation:
+    class Meta:
+        db_table = "userpostassociation"
+        verbose_name = "UserPostAssociations"
+        verbose_name_plural = "UserPostAssociation"
+
+    post = models.ForeignKey('Post',on_delete=models.CASCADE)
+    user = models.ForeignKey('Account',on_delete=models.CASCADE)
+
+        
